b6b87d87b2466a4e0797e677a4b2afe6f1e34210
BtbN <btbn@btbn.de>
2018-09-18 15:28:41 +0200

    Enable publishing for Linux and Windows

---
+++ FlagCarrierMini/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FlagCarrierMini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
