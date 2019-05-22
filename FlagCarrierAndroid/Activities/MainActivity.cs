86b4d90c7c8109a892386b8b054a210b656dbc4b
BtbN <btbn@btbn.de>
2019-05-23 00:46:02 +0200

    Bringup UI

--- FlagCarrierAndroid/Activities/MainActivity.cs
+++ FlagCarrierAndroid/Activities/MainActivity.cs
@@ -1,21 +1,29 @@
-﻿using System;
-
-using Android.App;
-using Android.Content.PM;
+﻿using Android.Content.PM;
 using Android.Runtime;
 using Android.Views;
 using Android.OS;
-using Plugin.CurrentActivity;
+using Android.App;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
 using Android.Support.V7.App;
 using Android.Support.V7.Widget;
 
+using Plugin.CurrentActivity;
+
+using FlagCarrierAndroid.Fragments;
+
+using SupportFragment = Android.Support.V4.App.Fragment;
+
 namespace FlagCarrierAndroid.Activities
 {
-    [Activity(Label = "FlagCarrierAndroid", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     [IntentFilter(new[] { "android.nfc.action.NDEF_DISCOVERED" }, Categories = new[] { "android.intent.category.DEFAULT" }, DataMimeType = FLAGCARRIER_MIMETYPE)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         public const string FLAGCARRIER_MIMETYPE = "application/vnd.de.oromit.flagcarrier";
+
+        private DrawerLayout drawer = null;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -28,13 +36,62 @@ namespace FlagCarrierAndroid.Activities
             Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-            SupportActionBar.Title = "@string/app_name";
+            drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+            navigationView.SetCheckedItem(Resource.Id.nav_scan_tag);
+
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content, new ScanTagFragment())
+                .Commit();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            SupportFragment fragment;
+
+            switch (item.ItemId)
+            {
+                case Resource.Id.nav_scan_tag:
+                    fragment = new ScanTagFragment();
+                    break;
+                case Resource.Id.nav_manual_login:
+                case Resource.Id.nav_write_tag:
+                case Resource.Id.nav_beam_mini:
+                case Resource.Id.nav_settings:
+                default:
+                    return false;
+            }
+
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content, fragment)
+                .Commit();
+
+            drawer.CloseDrawer(GravityCompat.Start);
+
+            return true;
+        }
+
+        public override void OnBackPressed()
+        {
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
         }
     }
 }
---
+++ FlagCarrierAndroid/Fragments/ScanTagFragment.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+
+using FlagCarrierAndroid.Helpers;
+
+namespace FlagCarrierAndroid.Fragments
+{
+    public class ScanTagFragment : Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.fragment_scan_tag, container, false);
+
+            Button clearButton = view.FindViewById<Button>(Resource.Id.clearButton);
+            clearButton.Click += ClearButton_Click;
+
+            return view;
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            PopUpHelper.Snackbar("I should clear stuff now...");
+        }
+    }
+}
