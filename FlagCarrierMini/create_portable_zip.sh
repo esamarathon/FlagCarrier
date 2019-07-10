967a4d07ba422b7111d765621f9703190a5f2699
BtbN <btbn@btbn.de>
2019-07-10 16:33:35 +0200

    Make exec script portable

--- FlagCarrierMini/create_portable_zip.sh
+++ FlagCarrierMini/create_portable_zip.sh
@@ -16,8 +16,8 @@ OF="${PWD}/bin/FlagCarrierMini.zip"
 mv bin/FlagCarrierMini /tmp/FlagCarrierMini || exit -2
 cd /tmp
 
-printf "@echo off\r\nstart dotnet FlagCarrierMini.dll\r\n" > FlagCarrierMini/FlagCarrierMini.cmd
-printf "#!/bin/sh\ndtn=dotnet\n! command -v dotnet >/dev/null 2>&1 && command -v dotnet.exe >/dev/null 2>&1 && dtn=dotnet.exe\nexec \$dtn FlagCarrierMini.dll\n" > FlagCarrierMini/FlagCarrierMini.sh
+printf '@echo off\r\nstart dotnet FlagCarrierMini.dll\r\n' > FlagCarrierMini/FlagCarrierMini.cmd
+printf '#!/bin/sh\ndtn=dotnet\n! command -v dotnet >/dev/null 2>&1 && command -v dotnet.exe >/dev/null 2>&1 && dtn=dotnet.exe\ncd "$(dirname "$0")"\nexec $dtn FlagCarrierMini.dll\n' > FlagCarrierMini/FlagCarrierMini.sh
 
 find FlagCarrierMini -type f -exec chmod 644 {} \;
 find FlagCarrierMini -type d -exec chmod 755 {} \;
